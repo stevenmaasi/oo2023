0d4ca401e21cd6455bab4eee2b3fe1899db9edb2
stevenmaasi <zteven@tlu.ee>
2023-03-16 22:00:58 +0200

    Minu mäng

---
+++ Game/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("See on minu mäng! - Tegelt testin niisama.");
+    }
+}
