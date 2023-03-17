4bdde9fd0915cb9621beec03c5b7b18baaf4e45b
stevenmaasi <zteven@tlu.ee>
2023-03-17 14:38:07 +0200

    Nüüd parem

--- 05_kontrolltoo1/KT1/src/Main.java
+++ 05_kontrolltoo1/KT1/src/Main.java
@@ -2,6 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         // Step max = alphabet.length() = 31
         int step = 31;
         String text = "Tere, minu nimi on Steven!";
+
         String inputFilePath = "F:\\OOP2023\\oo2023\\05_kontrolltoo1\\KT1\\src\\file.txt";
         String outputFile = "output.txt";
 
@@ -32,21 +34,21 @@ public class Main {
     }
 
     private static List<String> cipherFileText(String inputFilePath) {
-        List<String> lines = null;
+        List<String> cipheredLines = new ArrayList<>();
 
         try {
-            lines = readFile(Path.of(inputFilePath));
+            List<String> lines = readFile(Path.of(inputFilePath));
             int fileStep = Integer.parseInt(lines.get(0));
 
             for (int i = 1; i < lines.size(); i++) {
-                lines.set(i, Cipher.cipherText(lines.get(i), fileStep));
+                cipheredLines.add(i - 1, Cipher.cipherText(lines.get(i), fileStep));
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        return lines;
+        return cipheredLines;
     }
 
     private static void writeCipherToFile(String inputFilePath, String outputFile) {
@@ -56,11 +58,13 @@ public class Main {
             if (lines.size() > 0) {
                 FileWriter myWriter = new FileWriter(outputFile);
 
-                for (int i = 1; i < lines.size(); i++) {
-                    myWriter.write(lines.get(i) + '\n');
+                for (String line : lines) {
+                    myWriter.write(line + '\n');
                 }
 
                 myWriter.close();
+
+                System.out.println("New file written.");
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
