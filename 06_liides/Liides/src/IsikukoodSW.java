b9c48005de3749581112fe5272c6b14da4c37145
stevenmaasi <zteven@tlu.ee>
2023-05-04 22:41:09 +0300

    Automaattestid + isikukoodid tunnis

---
+++ 06_liides/Liides/src/IsikukoodSW.java
@@ -0,0 +1,21 @@
+public class IsikukoodSW extends Isikukood implements Andmed {
+
+    public IsikukoodSW(String nimi, String isikukood) {
+        super(nimi, isikukood);
+    }
+
+    @Override
+    public String getSugu() {
+        return null;
+    }
+
+    @Override
+    public String getKuupaev() {
+        return null;
+    }
+
+    @Override
+    public int getVanus() {
+        return 0;
+    }
+}
